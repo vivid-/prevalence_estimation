3d8c35adc451fdc16c708734b3cd22365adec81e
Wei Liu of group zhao <wl382@c14n06.farnam.hpc.yale.internal>
2018-05-15 22:37:23 -0400

    paramter estimation via empirical bayes

---
+++ prior_estimation/empirical_beta.R
@@ -0,0 +1,33 @@
+#!/usr/bin/R
+
+####################################################
+## this script is used to estimate the parameters ##
+## in the categorized beta distribution ############
+####################################################
+options(stingsAsFactos=F)
+args <- commandArgs()
+
+categorize_varaints <- function(tb,type){
+    ind <- which(dat$Consequence==type)
+    return(tb[ind,])
+}
+
+
+input <- args[6]
+file <- gzfile(input,"r")
+#file <- gzfile("/ysm-gpfs/scratch60/wl382/WES/Data/ExAC/ExAC.r0.3.1.sites.vep.canonical.table.gz","r")
+dat <- read.table(file,header=T,sep="\t")
+### first to filter varaints by PASS ###
+dat.passed <- dat[dat$FILTER=="PASS",]
+
+result_predix <- args[7]
+#result_predix <- "/ysm-gpfs/scratch60/wl382/WES/Data/ExAC/ExAC.r0.3.1.sites.vep.canonical"
+types <- c("frameshift_variant","splice_acceptor_variant","splice_donor_variant","stop_gained","missense_variant")
+#types <- c("missense_variant")
+for(type in types){
+	dat.tmp <- categorize_varaints(dat.passed,type)
+	# extract AC and AN #
+        dat.1 <- dat.tmp[,c('CHROM','POS','REF','ALT','ID','AN','AN_Adj','AC','AC_Adj')]
+	result_file <- paste0(result_predix,"_",type,".txt")
+	write.table(dat.1,result_file,col.names=T,row.names=F,quote=F,sep="\t")	
+}
---
+++ prior_estimation/get_beta_parameters.R
@@ -0,0 +1,45 @@
+#!/usr/bin/R
+
+######################################################
+# this script is used to estimate parameters in beta #
+# distribution of allele frequency for each category #
+# variants ###########################################
+######################################################
+args <- commandArgs()
+options(stringsAsFactors=F)
+
+empiri.parameters<- function(ACs,ANs){
+	qs <- as.numeric(ACs)/as.numeric(ANs)
+	estimated.mean = mean(qs)
+	eastimated.var = var(qs)
+	A = estimated.mean
+	B = eastimated.var
+	w = A-1 + sqrt((A*(1-A)^3)/B)
+	v = A/(1-A)*w
+	params = c(v,w)
+	return(params)
+}
+result <- data.frame()
+types <- c("frameshift_variant","splice_acceptor_variant","splice_donor_variant","stop_gained","missense_variant")
+
+input_predix <- args[6]
+output <- args[7]
+for(type in types){
+        dat <- read.table(paste0(input_predix,type,".txt"),header=T)
+	#dat <- read.table(paste0("/ysm-gpfs/scratch60/wl382/WES/Data/ExAC/ExAC.r0.3.1.sites.vep.canonical_",type,".txt"),header=T)
+	ACs = as.numeric(na.omit(dat$AC_Adj))
+	ANs = as.numeric(na.omit(dat$AN_Adj))
+	# get the minor allele counts 
+	minor.indx = which(ACs>ANs-ACs)
+	ACs[minor.indx] = ANs[minor.indx] - ACs[minor.indx]
+
+	estimated.params <- empiri.parameters(ACs,ANs)
+	tmp <- data.frame(type = type, v=estimated.params[1],w = estimated.params[2])
+	if(dim(result)[1]==0){
+		result <- tmp
+	}else{
+		result <- rbind(result,tmp)
+	}
+}
+
+write.table(result,file = output,col.names=T,row.names=F,quote=F,sep="\t")
