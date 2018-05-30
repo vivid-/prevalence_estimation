be5f3a5c8bba72aa89fd68c533cb2aba7474c8d3
Wei Liu of group zhao <wl382@c15n12.farnam.hpc.yale.internal>
2018-05-30 17:40:54 -0400

    updated scripts for prevalence calculation

--- prevalence/run_annot.R
+++ prevalence/run_annot.R
@@ -1,6 +1,6 @@
-genes <- c("FKRP","TCAP","ANO5","SGCA","CAPN3","SGCB","SGCD","SGCG","SGCA")
+genes <- c("FKRP","TCAP","ANO5","SGCA","CAPN3","SGCB","SGCD","SGCG","DYSF")
 sink("run_annot_allMis.sh")
 for(g in genes){
-	test <- paste0("Rscript annotate_pathogenecity_allMissense.R /ysm-gpfs/scratch60/wl382/WES/data/data/",g,"_mutation_AF.txt /ysm-gpfs/scratch60/wl382/WES/data/data/",g,"_mutation_AF_patho_allMissense.txt")
+	test <- paste0("Rscript annotate_pathogenecity_allMissense.R ../data/",g,"_mutation_AF.txt ../data/",g,"_mutation_AF_patho_allMissense.txt")
 	cat(test,"\n")
 }
--- prevalence/run_script.R
+++ prevalence/run_script.R
@@ -1,12 +1,12 @@
 #!/usr/bin/R
-genes <- c("DYSF")
-#genes <- c("FKRP","TCAP","ANO5","SGCA","CAPN3","SGCB","SGCD","SGCG","SGCA")
+#genes <- c("DYSF")
+genes <- c("FKRP","TCAP","ANO5","SGCA","CAPN3","SGCB","SGCD","SGCG","SGCA","DYSF")
 populations <- c("All","EUR","NFE","FIN")
 
 sink("run_test_allMissense.sh")
 for(g in genes){
 	for (p in populations){
-		test <- paste0("Rscript bayesian_estimation.R /ysm-gpfs/scratch60/wl382/WES/data/data/",g,"_mutation_AF_patho_allMissense.txt /ysm-gpfs/scratch60/wl382/WES/data/beta_parameter_prior_ExAC.txt ",p," 0.95 /ysm-gpfs/scratch60/wl382/WES/data/data/",g,"_",p,"_0.95_allMissense.txt")
+		test <- paste0("Rscript bayesian_estimation.R /ysm-gpfs/scratch60/wl382/WES/prevalence_estimation/prevalence_estimation/data/",g,"_mutation_AF_patho_allMissense.txt /ysm-gpfs/scratch60/wl382/WES/data/beta_parameter_prior_ExAC.txt ",p," 0.95 /ysm-gpfs/scratch60/wl382/WES/prevalence_estimation/prevalence_estimation/result/",g,"_",p,"_0.95_allMissense.txt")
 		cat(test,"\n")
 	}
 }
