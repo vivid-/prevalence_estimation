63fa6dee0a17d1dbc1863bcd1b7b4074058171ef
Wei Liu of group zhao <wl382@c12n02.farnam.hpc.yale.internal>
2018-06-13 01:06:04 -0400

    categorize more varaints

--- prior_estimation/empirical_beta.R
+++ prior_estimation/empirical_beta.R
@@ -6,9 +6,20 @@
 options(stingsAsFactos=F)
 args <- commandArgs()
 
-categorize_varaints <- function(tb,type){
+categorize_varaints <- function(tb,type,types){
     ind <- which(dat$Consequence==type)
-    return(tb[ind,])
+    if(length(ind)==0){
+	ind <- grep(type,dat$Consequence)
+    }
+    if(length(ind)==0){
+	ind.union <- c()
+	for(type in c("frameshift_variant","splice_acceptor_variant","splice_donor_variant","stop_gained","missense_variant","exon_variant","UTR_variant")){
+		ind.union <- c(ind.union,categorize_varaints(tb,type,types))
+	}	
+	return (setdiff(seq(1,dim(tb)[1],1),ind.union))
+	
+    }
+    return(ind)
 }
 
 
@@ -21,10 +32,10 @@ dat.passed <- dat[dat$FILTER=="PASS",]
 
 result_predix <- args[7]
 #result_predix <- "/ysm-gpfs/scratch60/wl382/WES/Data/ExAC/ExAC.r0.3.1.sites.vep.canonical"
-types <- c("frameshift_variant","splice_acceptor_variant","splice_donor_variant","stop_gained","missense_variant")
-#types <- c("missense_variant")
+#types <- c("frameshift_variant","splice_acceptor_variant","splice_donor_variant","stop_gained","missense_variant","exon_variant","UTR_variant","other_variant")
+types <- c("other_variant")
 for(type in types){
-	dat.tmp <- categorize_varaints(dat.passed,type)
+	dat.tmp <- dat.passed[categorize_varaints(dat.passed,type,types),]
 	# extract AC and AN #
         dat.1 <- dat.tmp[,c('CHROM','POS','REF','ALT','ID','AN','AN_Adj','AC','AC_Adj')]
 	result_file <- paste0(result_predix,"_",type,".txt")
