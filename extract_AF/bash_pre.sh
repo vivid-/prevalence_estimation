1daaf3c6d52558414ca27d139c3d6fee78e6343c
Wei Liu of group zhao <wl382@c18n04.farnam.hpc.yale.internal>
2018-06-14 20:56:38 -0400

    script for extracting AD

--- extract_AF/bash_pre.sh
+++ extract_AF/bash_pre.sh
@@ -5,29 +5,29 @@ end=$3
 gene=$4
 
 # 1. download the WGS and WES files from gnomAD
-wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/exomes/gnomad.exomes.r2.0.2.sites.vcf.bgz
-wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/exomes/gnomad.exomes.r2.0.2.sites.vcf.bgz.tbi
+#wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/exomes/gnomad.exomes.r2.0.2.sites.vcf.bgz
+#wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/exomes/gnomad.exomes.r2.0.2.sites.vcf.bgz.tbi
 wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr${chr}.vcf.bgz
 wget https://storage.googleapis.com/gnomad-public/release/2.0.2/vcf/genomes/gnomad.genomes.r2.0.2.sites.chr${chr}.vcf.bgz.tbi
 
 # 2. retrieve WGS and WES based on their genomic coordinates
-tabix -fh gnomad.exomes.r2.0.2.sites.vcf.bgz ${chr}:${sta}-${end} > ./data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}.vcf
-tabix -fh gnomad.genomes.r2.0.2.sites.chr${chr}.vcf.bgz  ${chr}:${sta}-${end} > ./data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}.vcf
+tabix -fh gnomad.exomes.r2.0.2.sites.vcf.bgz ${chr}:${sta}-${end} > ../data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}.vcf
+tabix -fh gnomad.genomes.r2.0.2.sites.chr${chr}.vcf.bgz  ${chr}:${sta}-${end} > ../data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}.vcf
 
 # 3. parse mutiple ATL alleles into different lines with just one ALT allele
-vcf_parser ./data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}.vcf --split -o ./data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.vcf
-vcf_parser ./data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}.vcf --split -o ./data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.vcf
+vcf_parser ../data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}.vcf --split -o ../data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.vcf
+vcf_parser ../data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}.vcf --split -o ../data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.vcf
 
 # 4. vcf2table
-Rscript vcf2table.R ./data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.vcf ${gene} ./data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.table 
-Rscript vcf2table.R ./data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.vcf ${gene} ./data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.table
+Rscript vcf2table.R ../data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.vcf ${gene} ../data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.table 
+Rscript vcf2table.R ../data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.vcf ${gene} ../data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.table
 
 # 5. mini represent vcf information
-python get_minimal_representation.py --INPUT ./data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.table --OUTPUT ./data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table
-python get_minimal_representation.py --INPUT ./data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.table --OUTPUT ./data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table
+python get_minimal_representation.py --INPUT ../data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.table --OUTPUT ../data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table
+python get_minimal_representation.py --INPUT ../data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted.table --OUTPUT ../data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table
 
 # 6. merge WES and WGS result
-Rscript merge_genome_exome.R ./data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table ./data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table ./data/gnomad.genomes_exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table
+Rscript merge_genome_exome.R ../data/gnomad.genomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table ../data/gnomad.exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table ../data/gnomad.genomes_exomes.r2.0.2.sites_${chr}.${sta}-${end}_${gene}_splitted_miniRepresented.table
 
 
 
