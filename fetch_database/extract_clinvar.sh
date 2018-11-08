0fe736440f456633e2fd2f216736d632dd9675b7
Wei Liu <8793240+vivid-@users.noreply.github.com>
2018-11-07 23:06:23 -0500

    Update extract_clinvar.sh

--- fetch_database/extract_clinvar.sh
+++ fetch_database/extract_clinvar.sh
@@ -1,12 +1,14 @@
-# fecth clinvar database file
-wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20180429.vcf.gz
-wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20180429.vcf.gz.tbi
-
-# extract variants based on their locations
 chr="$1"
 sta="$2"
 end="$3"
 gene="$4"
-tabix clinvar_20180429.vcf.gz ${chr}:${sta}-${end} > ./data/clinvar_${chr}_${sta}-${end}_${gene}.vcf
-Rscript extract_patho_annotation.R ./data/clinvar_${chr}_${sta}-${end}_${gene}.vcf ./data/clinvar_${chr}_${sta}-${end}_${gene}_info.table
+clinvar="$5"
+# fecth clinvar database file
+# clinvar_20180429
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar}.vcf.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar}.vcf.gz.tbi
+
+# extract variants based on their locations
+tabix ${clinvar}.vcf.gz ${chr}:${sta}-${end} > ../data/clinvar_${chr}_${sta}-${end}_${gene}.vcf
+Rscript extract_patho_annotation.R ../data/clinvar_${chr}_${sta}-${end}_${gene}.vcf ../data/clinvar_${chr}_${sta}-${end}_${gene}_info.table
 
