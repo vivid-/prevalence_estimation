e92676e39a37b74921ef76855e5504cdc98a4f83
Wei Liu <8793240+vivid-@users.noreply.github.com>
2019-01-01 22:22:17 -0500

    Update extract_clinvar.sh

--- fetch_database/extract_clinvar.sh
+++ fetch_database/extract_clinvar.sh
@@ -5,9 +5,12 @@ gene="$4"
 clinvar="$5"
 # fecth clinvar database file
 # clinvar_20180429
-wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar}.vcf.gz
-wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar}.vcf.gz.tbi
-
+{
+    wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar}.vcf.gz
+    wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar}.vcf.gz.tbi
+}||{
+    echo "the input for clivar version is not updated, go to the website ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/"
+}
 # extract variants based on their locations
 tabix ${clinvar}.vcf.gz ${chr}:${sta}-${end} > ../data/clinvar_${chr}_${sta}-${end}_${gene}.vcf
 Rscript extract_patho_annotation.R ../data/clinvar_${chr}_${sta}-${end}_${gene}.vcf ../data/clinvar_${chr}_${sta}-${end}_${gene}_info.table
