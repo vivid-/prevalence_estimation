79ea9c7bd4ba2b2638ede2c7118b0d679d1b616d
Wei Liu <8793240+vivid-@users.noreply.github.com>
2019-01-10 00:46:45 -0500

    Update extract_clinvar.sh

--- fetch_database/extract_clinvar.sh
+++ fetch_database/extract_clinvar.sh
@@ -2,15 +2,24 @@ chr="$1"
 sta="$2"
 end="$3"
 gene="$4"
-clinvar="$5"
+
+if [ "$#" -eq 5 ]; then
+    clinvar="$5"
+fi
+
+if [ "$#" -eq 6 ]; then
+    clinvar="$6"
+    clinvar_loc="$5"
+fi
+
 # fecth clinvar database file
 # clinvar_20180429
 {
     wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar}.vcf.gz
     wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar}.vcf.gz.tbi
 }||{
-    wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/archive_2.0/2018/${clinvar}.vcf.gz
-    wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/archive_2.0/2018/${clinvar}.vcf.gz.tbi
+    wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar_loc}/${clinvar}.vcf.gz
+    wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/${clinvar_loc}/${clinvar}.vcf.gz.tbi
 #    echo "the input for clivar version is not updated, go to the website ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/"
 }||{
     echo "the input for clivar version is not correct, go to the website ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/ or ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/archive_2.0/2018" 
